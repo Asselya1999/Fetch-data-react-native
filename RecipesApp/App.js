de9ba7f90a680d85bd922c0ee1fadc0f1b6f7437
Asselya1999 <bersinbaevaa@gmail.com>
2022-05-02 02:04:36 +0600

    commit

---
+++ RecipesApp/App.js
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';  
+import SearchMovies from './screens/SearchMovies'
+import MovieDetails from './screens/MovieDetails'
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import SafeAreaView from 'react-native-safe-area-view';
+ 
+const Stack = createStackNavigator()
+
+export default class App extends React.Component {
+ 
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name = "SearchMovies" 
+          component = {SearchMovies} 
+          options={{ title: 'Movie Browser' }} />
+          <Stack.Screen name = "MovieDetails" 
+          component = {MovieDetails} 
+          options={{ title: 'Movie Details' }} />
+        </ Stack.Navigator>
+      </ NavigationContainer>
+    );
+  }
+}
